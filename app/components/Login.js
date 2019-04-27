a3297b8e2bd4707cabb00b34c8c4e58f310cc5a6
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2019-04-27 08:00:12 -0300

    finalizando interface de Login e iniciando interface de cadastro

--- app/components/Login.js
+++ app/components/Login.js
@@ -3,7 +3,7 @@
   * Classe de controle do Login do aplicativo
   * Desenvolvido por Mateus Medeiros
   * https://github.com/mathmed
-  * Última atualização no arquivo: 26/04/2019
+  * Última atualização no arquivo: 27/04/2019
   * Projeto utilizando o framework React Native
   * Software desenvolvido para disciplina de Engenharia de Software II / UFRN
 */
@@ -11,10 +11,12 @@
 
 /* importações necessárias */
 import React, {Component} from 'react';
-import {Container, Footer, Content, Text, Label} from "native-base"; 
-import {StatusBar} from "react-native";
+import {Container, Footer, Content, Text, Label, View, Input, Item, Icon, Button} from "native-base";
+import {Image} from "react-native"; 
+import {StatusBar, TouchableOpacity} from "react-native";
 import { connect } from 'react-redux';
-import LottieView from 'lottie-react-native';
+import {Actions} from "react-native-router-flux";
+
 import styles from "../styles/styles";
 
 
@@ -28,14 +30,50 @@ class Login extends Component{
 
 	render(){
 		return(
-            <Container>
+            <Container style = {styles.primaryColorBack}>
 
-			    <StatusBar backgroundColor = '#007A88' />
-                <Content style = {styles.container}>
-                    <Text>eae</Text>
+			    <StatusBar backgroundColor = "#0f0f4c" />
+                <Content style = {[styles.container, styles.primaryColorBack]}>
+                    <View style = {styles.center}>
+                        <Image style = {styles.logo} source = {require("../assets/images/logo.png")} />                
+                    </View>
+
+                    <View style = {styles.bigMarginTop}>
+                        <Item>
+                            <Icon style = {styles.whiteColor} type = "FontAwesome5" name='user' />
+                            <Input style = {styles.whiteColor} placeholderTextColor = "white" placeholder='Usuário'/>
+                        </Item>
+                    </View>
+                    <View style = {styles.bigMarginTop}>
+                        <Item>
+                            <Icon style = {styles.whiteColor} type = "FontAwesome5" name='lock' />
+                            <Input style = {styles.whiteColor} placeholderTextColor = "white" secureTextEntry placeholder='Senha'/>
+                        </Item>
+                    </View>
+
+                    <View style = {[styles.bigMarginTop, styles.center]}>
+                        <Button iconRight style = {[styles.secundaryColorBack, {width: 250}]}>
+                            <Text style = {styles.bold}>ENTRAR</Text>
+                            <Icon type = "FontAwesome5" name='sign-in-alt' />
+                        </Button>
+                    </View>
+
+                    <View style = {[styles.marginTop, styles.center]}>
+                        <Button bordered iconRight style = {[styles.whiteColorBack, {width: 250}]}>
+                            <Text style = {[styles.secundaryColor, styles.bold]}>ENTRAR COM GOOGLE</Text>
+                            <Icon style = {styles.secundaryColor} type = "FontAwesome5" name='google' />
+                        </Button>
+                    </View>
+
+                    <View style = {[styles.bigMarginTop]}>
+                        <TouchableOpacity onPress = {() => Actions.criar_conta()}>
+                            <Text style = {[styles.whiteColor, styles.bold, styles.textCenter, styles.mediumFont]}>Não possui conta? crie agora!</Text>
+                        </TouchableOpacity>
+                    </View>
+
                 </Content>
-                <Footer style = {[styles.primaryColorBack, styles.header]}>
-                    <Label style = {[styles.whiteColor, styles.center, styles.bold]}>© Matechs 2019</Label>
+                <Footer style = {styles.primaryColorBack}>
+                    <Label style = {[styles.whiteColor, styles.center, styles.bold]}>© Minha Despensa 2019</Label>
                 </Footer>
 
             </Container>
--- app/styles/styles.js
+++ app/styles/styles.js
@@ -1,10 +1,55 @@
 
 /* 
-  * Classe de controle de estilos do aplicativo
+  * Arquivo de controle de estilos do aplicativo
   * Desenvolvido por Mateus Medeiros
   * https://github.com/mathmed
-  * Última atualização no arquivo: 26/04/2019
+  * Última atualização no arquivo: 27/04/2019
   * Projeto utilizando o framework React Native
   * Software desenvolvido para disciplina de Engenharia de Software II / UFRN
 */
 
+/* Importações necessárias */
+
+import {StyleSheet} from "react-native";
+
+export default styles = StyleSheet.create({
+
+  /* Cores */
+  primaryColor: {color: "#19197f"},
+  primaryColorBack: {backgroundColor: "#19197f"},
+  secundaryColor: {color: "#ff3333"},
+  secundaryColorBack: {backgroundColor: "#ff3333"},
+  whiteColor: {color: "white"},
+  whiteColorBack: {backgroundColor: "white"},
+  greyColor: {color:"#333333"},
+
+  /* Fontes */
+  smallFont: {fontSize: 14},
+  mediumFont: {fontSize: 20},
+  bigFont: {fontSize: 26},
+  titleFont: {fontSize: 30},
+  textCenter: {textAlign: "center"},
+  bold: {fontWeight: "bold"},
+  roboto: {fontFamily: "roboto"},
+
+  /* Views */
+  container: {backgroundColor: "white", flex: 1, paddingHorizontal: 15, marginTop: 10},
+  center: {justifyContent: "center", alignItems: "center", alignSelf: "center"},
+  column: {flexDirection: "column"},
+  row: {flexDirection: "row"},
+  spaceAround: {justifyContent: "space-around"},
+  spaceBetween: {justifyContent: "space-between"},
+  marginTop: {marginTop: 10},
+  bigMarginTop: {marginTop: 30},
+  smallMarginTop: {marginTop: 5},
+
+
+  /* Específicos */
+  viewTitleLogin: {backgroundColor:"#3333ff" },
+  logo: {width: 140, height: 140}
+
+
+
+
+
+});
